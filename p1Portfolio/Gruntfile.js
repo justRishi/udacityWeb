e24a9e6033d2805291926b0297edbb0e3e983a24
justRishi <justRishi@gmail.com>
2015-08-09 20:53:25 -0500

    secoond pages commit

---
+++ p1Portfolio/Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            name: 'small',
+            rename: 'none',
+            width: '50%',
+            quality: 40
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'img_src/',
+          dest: 'img/'
+        }]
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ['responsive_images']);
+
+};
