7e43a7bae664c2c26fad90315be3f7747b00355a
justRishi <justRishi@gmail.com>
2015-08-09 22:39:07 -0500

    correcting Stray end tag img

--- p1Portfolio/Gruntfile.js
+++ p1Portfolio/Gruntfile.js
@@ -7,10 +7,20 @@ module.exports = function(grunt) {
           engine: 'im',
           sizes: [{
             name: 'small',
-            rename: 'none',
-            width: '50%',
+            width: '300px',
             quality: 40
-          }]
+          },
+          {
+            name: 'medium',
+            width: '500px',
+            quality: 70
+          },
+          {
+            name: 'large',
+            width: '1000px',
+            quality: 80
+          }
+         ]
         },
         files: [{
           expand: true,
